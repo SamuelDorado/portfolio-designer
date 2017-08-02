d3880f654789ce19b177c8c2831bbfb5d27f7d2c
Samuel Dorado Rodríguez <samueldoradorodriguez@MacBook-Air-de-Samuel.local>
2017-08-02 22:06:18 +0200

    footer icons

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,51 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('gulp-autoprefixer');
+var uglify = require('gulp-uglify');
+var imagemin = require('gulp-imagemin');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var notify = require('gulp-notify');
+var uglifycss = require('gulp-uglifycss');
+var cache = require('gulp-cache');
 
 // Static server
 gulp.task('watch', function() {
     browserSync.init({
         server: {
-            baseDir: "./"
+            baseDir: "dist"
         }
     });
+    gulp.watch("*.html", ['copyHtml']);
+    gulp.watch("js/*.js", ['scripts']);
+    gulp.watch("css/*.css", ['css']);
     gulp.watch("*.html").on('change', browserSync.reload);
     gulp.watch("js/*.js").on('change', browserSync.reload);
     gulp.watch("css/*.css").on('change', browserSync.reload);
-    
 });
 
-gulp.task('default', ['watch']);
+// Concatenate & Minify JS
+gulp.task('scripts', function() {
+    return gulp.src('js/**/*')
+        .pipe(gulp.dest('dist/js'));
+});
+
+// Concatenate & Minify CSS
+gulp.task('css', function() {
+    return gulp.src('css/*.css')
+        .pipe(gulp.dest('dist/css'));
+});
+
+gulp.task('assets', function() {
+    return gulp.src('assets/**/*')
+        .pipe(gulp.dest('dist/assets'));
+});
+
+
+gulp.task('copyHtml', function() {
+    return gulp.src('index.html')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('build', ['scripts', 'css', 'assets', 'copyHtml']);
+gulp.task('default', ['scripts', 'css', 'assets', 'copyHtml', 'watch']);
